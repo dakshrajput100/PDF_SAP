ff32cbcc6c9d8e5b964a639613cb51509c8e3691
Daksh Kumar <daksh.k@sodalessolutions.com>
2025-12-22 13:33:25 +0000

    code pdf

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,52 +1,92 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    	"sap/ui/model/json/JSONModel",
+    "sap/ui/model/json/JSONModel",
 ],
-function (Controller,JSONModel) {
-    "use strict";
+    function (Controller, JSONModel) {
+        "use strict";
 
-    return Controller.extend("project2.controller.View1", {
-        onInit: function () {
-            var model = new JSONModel ( {});
-           	this.getView().setModel(model, "objectView");
-
-    
-        },
+        return Controller.extend("project2.controller.View1", {
+            onInit: function () {
+                var model = new JSONModel({});
+                this.getView().setModel(model, "objectView");
 
 
-			onPressPdfSunLifeForm: function () {
-				debugger
-    var objectView = this.getView().getModel("objectView");
-var Test = objectView.getProperty("/Test");
-    var dd = {
-        content: [
-            {
-                text: "To the best of your knowledge, why did the member stop working?",
-                style: "text",
-                margin: [0, 5, 0, 0]
             },
-            {
-                table: {
-                    widths: [507],
-                    heights: [20],
-                    body: [
-                        [
-                            {
-                                style: "text",
-                                text: Test || " ",
-                                border: [true, true, true, true]
+
+
+            onPressPdfSunLifeForm: function () {
+
+                var objectView = this.getView().getModel("objectView");
+                var Test = objectView.getProperty("/Test");
+                var dd = {
+                    content: [
+                        {
+                            text: "To the best of your knowledge, why did the member stop working?",
+                            style: "text",
+                            margin: [0, 5, 0, 0]
+                        },
+                        {
+                            table: {
+                                widths: [507],
+                                heights: [20],
+                                body: [
+                                    [
+                                        {
+                                            style: "text",
+                                            text: Test || " ",
+                                            border: [true, true, true, true]
+                                        }
+                                    ]
+                                ]
                             }
-                        ]
+                        }
                     ]
+                };
+
+                pdfMake.createPdf(dd).download("Sun Life Form.pdf");
+            },
+
+
+
+            onPressPdfSunLifeForm1: function () {
+
+                function safeBreak(str) {
+                    return (str || '').replace(/(.{30})/g, "$1\u200B");
                 }
+
+                var objectView = this.getView().getModel("objectView");
+                var Test = objectView.getProperty("/Test");
+
+                var dd = {
+                    content: [
+                        {
+                            text: "To the best of your knowledge, why did the member stop working?",
+                            style: "text",
+                            margin: [0, 5, 0, 0]
+                        },
+                        {
+                            table: {
+                                widths: [507],
+                                body: [
+                                    [
+                                        {
+                                            style: "text",
+                                            text: safeBreak(Test),
+                                            noWrap: false,
+                                            border: [true, true, true, true]
+                                        }
+                                    ]
+                                ]
+                            }
+                        }
+                    ]
+                };
+
+                pdfMake.createPdf(dd).download("Sun Life Form.pdf");
             }
-        ]
-    };
 
-    pdfMake.createPdf(dd).download("Sun Life Form.pdf");
-},
-	
 
-         	
+
+
+        });
     });
-});
